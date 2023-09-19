a9c700ae2be54e67dfe056e628275638f8f529f8
FreakLign <32242776+FreakLign@users.noreply.github.com>
2023-09-20 01:51:25 +0800

    update

--- Mustard.UI/Mustard.UI.Sunflower/Controls/ButtonStyle.xaml.cs
+++ Mustard.UI/Mustard.UI.Sunflower/Controls/ButtonStyle.xaml.cs
@@ -2,7 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace JinWu.EasyStyle;
+namespace Mustard.UI.Sunflower.Controls;
 
 public static class ButtonStyle
 {
@@ -10,32 +10,29 @@ public static class ButtonStyle
             DependencyProperty.RegisterAttached(
                 "MouseOverBackground",
                 typeof(SolidColorBrush),
-                typeof(Button),
-                new PropertyMetadata(new SolidColorBrush(Color.FromArgb(25,255,255,255))));
+                typeof(ButtonStyle),
+                new PropertyMetadata(new SolidColorBrush(Color.FromArgb(0xFF, 0x3C, 0x7F, 0xB1))));
     public static DependencyProperty MouseOverForegroundProperty =
             DependencyProperty.RegisterAttached(
                 "MouseOverForeground",
                 typeof(SolidColorBrush),
-                typeof(Button));
+                typeof(ButtonStyle));
+    public static DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "CornerRadius",
+                typeof(double),
+                typeof(ButtonStyle),
+                new PropertyMetadata(10.0));
 
-    public static void SetMouseOverBackground(Button button, SolidColorBrush color)
-    {
-        button.SetValue(MouseOverBackgroundProperty, color);
-    }
+    public static void SetMouseOverBackground(Button button, SolidColorBrush color) => button.SetValue(MouseOverBackgroundProperty, color);
 
-    public static SolidColorBrush GetMouseOverBackground(Button button)
-    {
-        return (SolidColorBrush)button.GetValue(MouseOverBackgroundProperty);
-    }
+    public static SolidColorBrush GetMouseOverBackground(Button button) => (SolidColorBrush)button.GetValue(MouseOverBackgroundProperty);
 
-    public static void SetMouseOverForeground(Button button, SolidColorBrush color)
-    {
-        button.SetValue(MouseOverForegroundProperty, color);
-    }
+    public static void SetMouseOverForeground(Button button, SolidColorBrush color) => button.SetValue(MouseOverForegroundProperty, color);
 
-    public static SolidColorBrush GetMouseOverForeground(Button button)
-    {
-        return (SolidColorBrush)button.GetValue(MouseOverForegroundProperty);
-    }
+    public static SolidColorBrush GetMouseOverForeground(Button button) => (SolidColorBrush)button.GetValue(MouseOverForegroundProperty);
 
+    public static double GetCornerRadius(Button button) => (double)button.GetValue(CornerRadiusProperty);
+
+    public static void SetCornerRadius(Button button, double value) => button.SetValue(CornerRadiusProperty, value);
 }
